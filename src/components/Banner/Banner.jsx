e8b38afcbd338d4bdd27877e63b76dd2afac152d
Dennis Peter <peterdennis573@gmail.com>
2024-03-13 10:15:47 +0300

    Worked on the banner div

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,11 +1,18 @@
-import "./Banner.css";
 import "../../components/Banner/Banner";
+import BannerImage from "../../components/../assets/images2/about.jpg";
+import "./Banner.css";
 const Banner = () => {
   return (
     <div className="banner">
-      <div className="container">
-        
-      </div>
+        <img src={BannerImage} className="banner-image"/>
+        <div className="banner-overlay">
+        <div className="container">
+        <p className="page-name-text">About Us</p>
+          <p className="page-expectation">
+            About Us - Who We Are?
+          </p>
+        </div>
+        </div>
     </div>
   );
 };
